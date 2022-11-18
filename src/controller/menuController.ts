73eafab127158daa21e9778ee5338a97efb3e84d
sunseo18 <sunseo1818@gmail.com>
2022-11-18 19:47:23 +0900

    [add] 컨트롤러, 모듈 틀

---
+++ src/controller/menuController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { menuService } from "../service";
+
+//~ 카테고리 별 메뉴 조회
+const getMenuByCategory = async (req: Request, res: Response) => {};
+
+//~ 메뉴 상세 조회
+const getMenuInfo = async (req: Request, res: Response) => {};
+
+const menuController = {
+  getMenuByCategory,
+  getMenuInfo,
+};
+
+export default menuController;
