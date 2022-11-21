9a90529b9f28efda0c0008e7ff9da6dc0f1d1585
sunseo18 <sunseo1818@gmail.com>
2022-11-22 00:17:57 +0900

    [add] GET /cart 장바구니 조회 api 구현

--- src/controller/cartController.ts
+++ src/controller/cartController.ts
@@ -3,7 +3,16 @@ import { Request, Response } from 'express';
 import { cartService } from '../service';
 
 //~ 장바구니 조회
-const getCart = async (req: Request, res: Response) => { };
+const getCart = async (req: Request, res: Response) => {
+    try {
+        const result = await cartService.getCart();
+        if (!result) return res.status(200).send({ status: 200, message: '담은 메뉴가 없습니다.' })
+        return res.status(200).send({ status: 200, message: '장바구니 조회 성공', data: result });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({ status: 500, message: '서버 내부 에러' })
+    }
+};
 
 //~ 장바구니 담기
 const postCart = async (req: Request, res: Response) => {
@@ -11,11 +20,11 @@ const postCart = async (req: Request, res: Response) => {
     const { menuId } = req.params;
     try {
         const result = await cartService.postCart(+menuId, cartpostRequestDto);
-        return res.status(200).send({ message: '장바구니 담기 성공', data: result });
+        return res.status(200).send({ status: 200, message: '장바구니 담기 성공', data: result });
 
     } catch (error) {
         console.log(error);
-        return res.status(500).send({ message: '서버 내부 에러' });
+        return res.status(500).send({ status: 500, message: '서버 내부 에러' });
     }
 };
 
